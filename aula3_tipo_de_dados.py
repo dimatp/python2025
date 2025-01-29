60a483b61528270ad91a01dfa38f3245aade6cbd
Timóteo Dima Pereira <timoteo5@icloud.com>
2025-01-28 22:36:22 -0300

    Tipo de dados, função print

---
+++ aula3_tipo_de_dados.py
@@ -0,0 +1,23 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Timóteo Dima Pereira')
+
+print(1, 'Timóteo "Dima"')
+
+# Aspas duplas
+print("Timóteo Dima")
+print(2, "Timóteo Dima'")
+
+# Escape
+print("Timóteo \"Dima\"")
+
+# r
+print(r"Timóteo \"Dima\"")
